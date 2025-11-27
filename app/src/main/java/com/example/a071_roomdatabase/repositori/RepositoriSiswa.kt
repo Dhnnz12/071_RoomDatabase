146e43315d44dd13846ce7c03503379bc93d444e
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-27 13:16:37 +0700

    Menambahkan interface repositori siswa dan class offlinerepositorisiswa

---
+++ app/src/main/java/com/example/a071_roomdatabase/repositori/RepositoriSiswa.kt
@@ -0,0 +1,22 @@
+package com.example.a071_roomdatabase.repositori
+
+import com.example.a071_roomdatabase.room.Siswa
+import com.example.a071_roomdatabase.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+
+): RepositoriSiswa{
+override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao
+    .getAllSiswa()
+override suspend fun insertSiswa(siswa: Siswa) = siswaDao
+    .Insert(siswa)
+}
