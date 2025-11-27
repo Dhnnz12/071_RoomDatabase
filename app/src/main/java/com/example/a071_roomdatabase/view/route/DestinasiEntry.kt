9125f7b8873f16e665b46c7de4cd58a0dea1d55e
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-27 13:32:33 +0700

    menambahkan objext destinasientry

--- app/src/main/java/com/example/a071_roomdatabase/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/a071_roomdatabase/view/route/DestinasiEntry.kt
@@ -1,4 +1,9 @@
 package com.example.a071_roomdatabase.view.route
 
-object DestinasiEntry {
+import com.example.a071_roomdatabase.R
+
+object DestinasiEntry : DestinasiNavigasi {
+
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
