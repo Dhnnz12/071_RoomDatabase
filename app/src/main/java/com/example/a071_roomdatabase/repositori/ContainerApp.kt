8f2b1184964bab821173a1a6c7c373f0692305d2
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-27 13:20:22 +0700

    Menambahkan class containerdataapp

--- app/src/main/java/com/example/a071_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/a071_roomdatabase/repositori/ContainerApp.kt
@@ -1,5 +1,18 @@
 package com.example.a071_roomdatabase.repositori
 
+import android.content.Context
+import com.example.a071_roomdatabase.room.DatabaseSiswa
+
 interface ContainerApp{
     val repositoriSiswa : RepositoriSiswa
 }
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp{
+            override val repositoriSiswa: RepositoriSiswa by lazy {
+                OfflineRepositoriSiswa(
+                    DatabaseSiswa.getDatabase(context).siswaDao()
+                )
+            }
+        }
+
