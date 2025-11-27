73d1a53007236c0570fede349600d14ef1cb7ee3
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-27 15:07:44 +0700

    menambahkan fun entrysiswabody dan column

--- app/src/main/java/com/example/a071_roomdatabase/view/HalamanEntry.kt
+++ app/src/main/java/com/example/a071_roomdatabase/view/HalamanEntry.kt
@@ -1,21 +1,30 @@
 package com.example.a071_roomdatabase.view
 
+import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.a071_roomdatabase.view.route.DestinasiEntry
+import com.example.a071_roomdatabase.viewmodel.provider.DetailSiswa
 import com.example.a071_roomdatabase.viewmodel.provider.EntryViewModel
 import com.example.a071_roomdatabase.viewmodel.provider.PenyediaViewModel
+import com.example.a071_roomdatabase.viewmodel.provider.UIStateSiswa
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -51,5 +60,41 @@ fun EntrySiswaScreen(
                 .fillMaxWidth()
         )
     }
+}
+
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa: UIStateSiswa,
+    onSiswaValueChange: (DetailSiswa) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        FormInputSiswa(
+            detailSiswa = uiStateSiswa.detailSiswa,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            enabled = uiStateSiswa.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.btn_submit))
+        }
+    }
+}
+
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    onValueChange: (DetailSiswa) -> Unit,
+    modifier: Modifier
+) {
+    TODO("Not yet implemented")
 }
 
