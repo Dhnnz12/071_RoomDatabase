686291bdf5f8702e2125dd6870b4b117e9b662ec
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-27 15:36:29 +0700

    menambahkan fun HostNavigasi

--- app/src/main/java/com/example/a071_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a071_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,33 @@
 package com.example.a071_roomdatabase.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.a071_roomdatabase.view.EntrySiswaScreen
+import com.example.a071_roomdatabase.view.HomeScreen
+import com.example.a071_roomdatabase.view.route.DestinasiEntry
+import com.example.a071_roomdatabase.view.route.DestinasiHome
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+
+    }
+}
+
