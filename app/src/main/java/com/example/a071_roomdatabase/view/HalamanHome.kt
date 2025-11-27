6132e3a002159c013fddd954a9b988491ca1de80
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-27 15:25:13 +0700

    menambahkan fun ListSiswa

--- app/src/main/java/com/example/a071_roomdatabase/view/HalamanHome.kt
+++ app/src/main/java/com/example/a071_roomdatabase/view/HalamanHome.kt
@@ -3,6 +3,8 @@ package com.example.a071_roomdatabase.view
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -95,7 +97,22 @@ fun BodyHome(
 }
 
 @Composable
-fun ListSiswa(itemSiswa: List<Siswa>, modifier: Modifier) {
+fun ListSiswa(
+    itemSiswa : List<Siswa>,
+    modifier: Modifier=Modifier
+){
+    LazyColumn(modifier = Modifier){
+        items(items = itemSiswa, key = {it.id}){
+                person ->  DataSiswa(
+            siswa = person,
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.padding_small)))
+        }
+    }
+}
+
+@Composable
+fun DataSiswa(siswa: Siswa, modifier: Modifier) {
     TODO("Not yet implemented")
 }
 
