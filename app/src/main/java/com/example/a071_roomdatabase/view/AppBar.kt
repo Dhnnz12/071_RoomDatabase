53193f32764fcc9fd20ac4bd0ff8dea6059cd243
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-27 14:59:47 +0700

    menambahkan fun siswatopaapbar

--- app/src/main/java/com/example/a071_roomdatabase/view/AppBar.kt
+++ app/src/main/java/com/example/a071_roomdatabase/view/AppBar.kt
@@ -1,2 +1,27 @@
 package com.example.a071_roomdatabase.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        })
+}
